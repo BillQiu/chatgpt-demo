020666ec9b6f4f191cb870be44cb452ae3fe126c
Bill Qiu <fanglaiq@gmail.com>
2023-03-07 01:42:01 +0800

    fix: refactor scroll to bottom logic in Generator component

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -54,10 +54,10 @@ export default () => {
         if (char) {
           setCurrentAssistantMessage(currentAssistantMessage() + char);
         }
-        window.scrollTo({
-          top: document.body.scrollHeight,
-          behavior: "smooth",
-        });
+        // window.scrollTo({
+        //   top: document.body.scrollHeight,
+        //   behavior: "smooth",
+        // });
       }
       done = readerDone;
     }
