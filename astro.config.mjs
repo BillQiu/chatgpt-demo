71ec8feec48619adbbbae76b2bedac7f2db04ec5
Bill Qiu <fanglaiq@gmail.com>
2023-03-12 10:18:31 +0800

    Remove Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,10 @@ import vercelDisableBlocks from "./plugins/vercelDisableBlocks";
 
 import node from "@astrojs/node";
 import vercel from "@astrojs/vercel/edge";
-import netlify from "@astrojs/netlify/edge-functions";
 
 const envAdapter = () => {
   if (process.env.OUTPUT == "vercel") {
     return vercel();
-  } else if (process.env.OUTPUT == "netlify") {
-    return netlify();
   } else {
     return node({
       mode: "standalone",
